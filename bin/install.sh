d0d92ddc3f3f59ff398480dc4cba53433e8b1ffd
AlexanderC <alexander.moldova@gmail.com>
2014-12-15 14:37:47 +0200

    improve installation script

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 PHP=$(which php)
+
+if [ ! -x ${PHP} ]; then
+    echo "No PHP executable found"
+    exit 1
+fi
+
+IS_PHP_OK=$(${PHP} -r "echo (int) version_compare(PHP_VERSION, '5.5.0', '>=');")
+
+if [ 0 == ${IS_PHP_OK} ]; then
+    echo "Consider updating PHP at least to PHP v5.5"
+    exit 1
+fi
+
 BINARY_PATH=$(dirname ${PHP})
 CSL_BINARY="https://raw.githubusercontent.com/AlexanderC/Consolator/master/build/consolator.phar"
 CSL_INCLUDE="https://raw.githubusercontent.com/AlexanderC/Consolator/master/build/consolator.noshebang.phar"
@@ -14,3 +27,4 @@ rm -f ${BINARY_PATH}"/consolator"
 curl -XGET ${CSL_BINARY} --output ${BINARY_PATH}"/consolator" --progress-bar --insecure
 chmod a+x ${BINARY_PATH}"/consolator"
 echo "  Done!"
+exit 0
