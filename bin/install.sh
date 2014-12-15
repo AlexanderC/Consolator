49f0e0bbe6d5c8ade2885665d5309700dda1eca6
AlexanderC <alexander.moldova@gmail.com>
2014-12-15 15:06:27 +0200

    some installation script updates

--- bin/install.sh
+++ bin/install.sh
@@ -20,12 +20,18 @@ CSL_BINARY="https://raw.githubusercontent.com/AlexanderC/Consolator/master/build
 CSL_INCLUDE="https://raw.githubusercontent.com/AlexanderC/Consolator/master/build/consolator.noshebang.phar?___="${UUID}
 INCLUDE_PATH=$(${PHP} -r "echo rtrim(explode(':', ltrim(get_include_path(), '.:'))[0], '/');")
 
+# add library
+echo "+ Remote library path "${CSL_INCLUDE}
 echo "+ Adding consolator.phar to include path "${INCLUDE_PATH}
 rm -f ${INCLUDE_PATH}"/consolator.phar"
 curl -XGET ${CSL_INCLUDE} --output ${INCLUDE_PATH}"/consolator.phar" --progress-bar --insecure
-echo "+ Adding consolator to the binaries path "${BINARY_PATH}
+
+# add binary
+echo "+ Remote binary path "${CSL_BINARY}
+echo "+ Adding consolator to the binaries path "${BINARY_PATH}" using binary url "${CSL_BINARY}
 rm -f ${BINARY_PATH}"/consolator"
 curl -XGET ${CSL_BINARY} --output ${BINARY_PATH}"/consolator" --progress-bar --insecure
 chmod a+x ${BINARY_PATH}"/consolator"
+
 echo "  Done!"
 exit 0
